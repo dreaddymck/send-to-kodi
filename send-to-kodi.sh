7c0891319b11e5c7f9ebfff82a97013b612a98e5
dreaddymck <dreaddymck@users.noreply.github.com>
2023-11-30 22:42:09 -0500

    refractor

--- send-to-kodi.sh
+++ send-to-kodi.sh
@@ -2,68 +2,8 @@
 
 SEND_TO_KODI_DIR="$(dirname "$(readlink -f "$0")")"
 # echo "$SEND_TO_KODI_DIR" >&2
-source "$SEND_TO_KODI_DIR/lib/source"
 
-while [[ $* ]]; do
-    case "$1" in
-    -h | --help)
-        show_help
-        exit
-        ;;
-    -s | --stop)
-        kodi_stop
-        exit
-        ;;
-    -n | --next)
-        kodi_next
-        exit
-        ;;
-    --pause)
-        kodi_pause
-        exit
-        ;;
-    --shutdown)
-        kodi_shutdown
-        exit
-        ;;        
-    --reboot)
-        kodi_reboot
-        exit
-        ;;
-    --iptv)
-        iptv_main
-        exit
-        ;;
-    -v | --version)
-        version
-        exit
-        ;;        
-    -d)
-        DOWNLOAD_DIR="$2"
-        shift
-        ;;
-    -l)
-        SHARE_PORT="$2"
-        shift
-        ;;
-    -r)
-        REMOTE="$2"
-        shift
-        ;;
-    -u)
-        LOGIN="$2"
-        shift
-        ;;
-    -x) SEND_RAW=1 ;;
-    -y) KODI_YOUTUBE=1 ;;
-    -g) GUI=1 ;;
-    -*) error "Unknown flag: $1" ;;
-    *)
-        INPUT="$1"
-        ;;
-    esac
-    shift
-done
+source "$SEND_TO_KODI_DIR/lib/source"
 
 [[ $REMOTE ]] || error "Kodi remote address NOT specified, see --help"
 
